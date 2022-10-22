c5f0c89675d642084c1b93d697695fbb1ae0c25e
OutboundSpade <44787842+OutboundSpade@users.noreply.github.com>
2022-10-22 18:09:39 -0400

    add project

---
+++ tmp_dir/other.h
@@ -0,0 +1,4 @@
+#ifndef OTHER_H
+#define OTHER_H
+int main2();
+#endif
---
+++ tmp_dir/payload.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "other.h"
+
+int main()
+{
+	printf("Ha ha! code injected!\n");
+	main2();
+	return 0;
+}
